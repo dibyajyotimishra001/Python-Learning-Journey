c72466dd7cc19b54830c86c4afd8126edd961b77
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-05-28 17:39:26 +0530

    file moved

--- Quick Questions/problem5.py
+++
@@ -1,8 +0,0 @@
-def sum_of_elements(item, idx = 0):
-    if idx == len(item):
-        return 0
-    value = item[idx] + sum_of_elements(item, idx + 1)
-    return value
-
-list_1 = [1, 2, 3, 4, 5]
-print(f"Here is sum of the list {list_1} = {sum_of_elements(list_1)}")
